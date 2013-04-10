20259bce2fd8fec2bcf0f155cee8c3d7f882aec9
Ritch Melton <Ritch Melton@localhost>
2013-04-10 18:38:58 -0400

    Added class stub and initial test case. Configured NCrunch

---
+++ BankOCR/OCRConverter.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace BankOCR
+{
+    public class OCRConverter
+    {
+        public int[] Convert(string testValue)
+        {
+            return Enumerable.Empty<int>().ToArray();
+        }
+    }
+}
