77aef061f3c03d6fb56ab840fc07a34279ead1d2
Ritch Melton <Ritch Melton@localhost>
2013-04-10 20:37:10 -0400

    Implemented conversion code. Started fixing tests.

--- BankOCR.Tests/OCRConversionTests.cs
+++ BankOCR.Tests/OCRConversionTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 
 namespace BankOCR.Tests
 {
+
     [TestFixture]
     public class OCRConversionTests
     {
@@ -10,148 +11,276 @@ namespace BankOCR.Tests
         public void AllZeroesTest()
         {
             string testValue =
-                 "_  _  _  _  _  _  _  _  _ " + Environment.NewLine +
-                "| || || || || || || || || |" + Environment.NewLine +
-                "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine;
+            " _  _  _  _  _  _  _  _  _ " + Environment.NewLine +
+            "| || || || || || || || || |" + Environment.NewLine +
+            "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine +
+            "                           " + Environment.NewLine;
 
             var converter = new OCRConverter();
             var result = converter.Convert(testValue);
-            Assert.That(result, Is.EqualTo(000000000));
+            Assert.That(result.ToValue(), Is.EqualTo(000000000));
+        }
+
+        [Test]
+        public void AllOnesTest()
+        {
+            string testValue =
+            "                           " + Environment.NewLine +
+            "  |  |  |  |  |  |  |  |  |" + Environment.NewLine +
+            "  |  |  |  |  |  |  |  |  |" + Environment.NewLine +
+            "                           " + Environment.NewLine;
+
+            var converter = new OCRConverter();
+            var result = converter.Convert(testValue);
+            Assert.That(result.ToValue(), Is.EqualTo(111111111));
+        }
+        [Test]
+        public void AllTwoesTest()
+        {
+            string testValue =
+            " _  _  _  _  _  _  _  _  _ " + Environment.NewLine +
+            "| || || || || || || || || |" + Environment.NewLine +
+            "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine +
+            "                           ";
+
+            var converter = new OCRConverter();
+            var result = converter.Convert(testValue);
+            Assert.That(result, Is.EqualTo(2222222222));
+        }
+        [Test]
+        public void AllThreesTest()
+        {
+            string testValue =
+            " _  _  _  _  _  _  _  _  _ " + Environment.NewLine +
+            "| || || || || || || || || |" + Environment.NewLine +
+            "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine +
+            "                           ";
+
+            var converter = new OCRConverter();
+            var result = converter.Convert(testValue);
+            Assert.That(result, Is.EqualTo(333333333));
+        }
+        [Test]
+        public void AllFoursTest()
+        {
+            string testValue =
+            " _  _  _  _  _  _  _  _  _ " + Environment.NewLine +
+            "| || || || || || || || || |" + Environment.NewLine +
+            "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine +
+            "                           ";
+
+            var converter = new OCRConverter();
+            var result = converter.Convert(testValue);
+            Assert.That(result, Is.EqualTo(444444444));
+        }
+        [Test]
+        public void AllFivesTest()
+        {
+            string testValue =
+            " _  _  _  _  _  _  _  _  _ " + Environment.NewLine +
+            "| || || || || || || || || |" + Environment.NewLine +
+            "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine +
+            "                           ";
+
+            var converter = new OCRConverter();
+            var result = converter.Convert(testValue);
+            Assert.That(result, Is.EqualTo(555555555));
+        }
+        [Test]
+        public void AllSixesTest()
+        {
+            string testValue =
+            " _  _  _  _  _  _  _  _  _ " + Environment.NewLine +
+            "| || || || || || || || || |" + Environment.NewLine +
+            "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine +
+            "                           ";
+
+            var converter = new OCRConverter();
+            var result = converter.Convert(testValue);
+            Assert.That(result, Is.EqualTo(666666666));
+        }
+        [Test]
+        public void AllSevensTest()
+        {
+            string testValue =
+            " _  _  _  _  _  _  _  _  _ " + Environment.NewLine +
+            "| || || || || || || || || |" + Environment.NewLine +
+            "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine +
+            "                           ";
+
+            var converter = new OCRConverter();
+            var result = converter.Convert(testValue);
+            Assert.That(result, Is.EqualTo(777777777));
+        }
+
+        [Test]
+        public void AllEightsTest()
+        {
+            string testValue =
+            " _  _  _  _  _  _  _  _  _ " + Environment.NewLine +
+            "| || || || || || || || || |" + Environment.NewLine +
+            "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine +
+            "                           ";
+
+            var converter = new OCRConverter();
+            var result = converter.Convert(testValue);
+            Assert.That(result, Is.EqualTo(888888888));
+        }
+
+        [Test]
+        public void AllNinesTest()
+        {
+            string testValue =
+            " _  _  _  _  _  _  _  _  _ " + Environment.NewLine +
+            "| || || || || || || || || |" + Environment.NewLine +
+            "|_||_||_||_||_||_||_||_||_|" + Environment.NewLine +
+            "                           ";
+
+            var converter = new OCRConverter();
+            var result = converter.Convert(testValue);
+            Assert.That(result, Is.EqualTo(999999999));
+        }
+    }
+
+    public static class ArrayExtension
+    {
+        public static int ToValue(this int[] array)
+        {
+            var result = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                result += array[i]*(int)Math.Pow(10,i);
+            }
+            return result;
         }
     }
 }
 
 /*
-  use case 1
-                           
-=> 000000000
-                           
-  |  |  |  |  |  |  |  |  |
-  |  |  |  |  |  |  |  |  |
-                           
-=> 111111111
+se case 1
+=&gt; 000000000
+=&gt; 111111111
  _  _  _  _  _  _  _  _  _ 
  _| _| _| _| _| _| _| _| _|
 |_ |_ |_ |_ |_ |_ |_ |_ |_ 
                            
-=> 222222222
+=&gt; 222222222
  _  _  _  _  _  _  _  _  _ 
  _| _| _| _| _| _| _| _| _|
  _| _| _| _| _| _| _| _| _|
                            
-=> 333333333
+=&gt; 333333333
                            
 |_||_||_||_||_||_||_||_||_|
   |  |  |  |  |  |  |  |  |
                            
-=> 444444444
+=&gt; 444444444
  _  _  _  _  _  _  _  _  _ 
 |_ |_ |_ |_ |_ |_ |_ |_ |_ 
  _| _| _| _| _| _| _| _| _|
                            
-=> 555555555
+=&gt; 555555555
  _  _  _  _  _  _  _  _  _ 
 |_ |_ |_ |_ |_ |_ |_ |_ |_ 
 |_||_||_||_||_||_||_||_||_|
                            
-=> 666666666
+=&gt; 666666666
  _  _  _  _  _  _  _  _  _ 
   |  |  |  |  |  |  |  |  |
   |  |  |  |  |  |  |  |  |
                            
-=> 777777777
+=&gt; 777777777
  _  _  _  _  _  _  _  _  _ 
 |_||_||_||_||_||_||_||_||_|
 |_||_||_||_||_||_||_||_||_|
                            
-=> 888888888
+=&gt; 888888888
  _  _  _  _  _  _  _  _  _ 
 |_||_||_||_||_||_||_||_||_|
  _| _| _| _| _| _| _| _| _|
                            
-=> 999999999
+=&gt; 999999999
     _  _     _  _  _  _  _
   | _| _||_||_ |_   ||_||_|
   ||_  _|  | _||_|  ||_| _| 
                            
-=> 123456789
+=&gt; 123456789
 
 use case 3
  _  _  _  _  _  _  _  _    
 | || || || || || || ||_   |
 |_||_||_||_||_||_||_| _|  |
                            
-=> 000000051
+=&gt; 000000051
     _  _  _  _  _  _     _ 
 |_||_|| || ||_   |  |  | _ 
   | _||_||_||_|  |  |  | _|
                            
-=> 49006771? ILL
+=&gt; 49006771? ILL
     _  _     _  _  _  _  _ 
   | _| _||_| _ |_   ||_||_|
   ||_  _|  | _||_|  ||_| _ 
                             
-=> 1234?678? ILL
+=&gt; 1234?678? ILL
 
 use case 4
                            
   |  |  |  |  |  |  |  |  |
   |  |  |  |  |  |  |  |  |
                            
-=> 711111111
+=&gt; 711111111
  _  _  _  _  _  _  _  _  _ 
   |  |  |  |  |  |  |  |  |
   |  |  |  |  |  |  |  |  |
                            
-=> 777777177
+=&gt; 777777177
  _  _  _  _  _  _  _  _  _ 
  _|| || || || || || || || |
 |_ |_||_||_||_||_||_||_||_|
                            
-=> 200800000
+=&gt; 200800000
  _  _  _  _  _  _  _  _  _ 
  _| _| _| _| _| _| _| _| _|
  _| _| _| _| _| _| _| _| _|
                            
-=> 333393333 
+=&gt; 333393333 
  _  _  _  _  _  _  _  _  _ 
 |_||_||_||_||_||_||_||_||_|
 |_||_||_||_||_||_||_||_||_|
                            
-=> 888888888 AMB ['888886888', '888888880', '888888988']
+=&gt; 888888888 AMB ['888886888', '888888880', '888888988']
  _  _  _  _  _  _  _  _  _ 
 |_ |_ |_ |_ |_ |_ |_ |_ |_ 
  _| _| _| _| _| _| _| _| _|
                            
-=> 555555555 AMB ['555655555', '559555555']
+=&gt; 555555555 AMB ['555655555', '559555555']
  _  _  _  _  _  _  _  _  _ 
 |_ |_ |_ |_ |_ |_ |_ |_ |_ 
 |_||_||_||_||_||_||_||_||_|
                            
-=> 666666666 AMB ['666566666', '686666666']
+=&gt; 666666666 AMB ['666566666', '686666666']
  _  _  _  _  _  _  _  _  _ 
 |_||_||_||_||_||_||_||_||_|
  _| _| _| _| _| _| _| _| _|
                            
-=> 999999999 AMB ['899999999', '993999999', '999959999']
+=&gt; 999999999 AMB ['899999999', '993999999', '999959999']
     _  _  _  _  _  _     _ 
 |_||_|| || ||_   |  |  ||_ 
   | _||_||_||_|  |  |  | _|
                            
-=> 490067715 AMB ['490067115', '490067719', '490867715']
+=&gt; 490067715 AMB ['490067115', '490067719', '490867715']
     _  _     _  _  _  _  _ 
  _| _| _||_||_ |_   ||_||_|
   ||_  _|  | _||_|  ||_| _| 
                            
-=> 123456789
+=&gt; 123456789
  _     _  _  _  _  _  _    
 | || || || || || || ||_   |
 |_||_||_||_||_||_||_| _|  |
                            
-=> 000000051
+=&gt; 000000051
     _  _  _  _  _  _     _ 
 |_||_|| ||_||_   |  |  | _ 
   | _||_||_||_|  |  |  | _|
                            
-=> 490867715   
- */
+=&gt; 490867715  */
--- BankOCR/OCRConverter.cs
+++ BankOCR/OCRConverter.cs
@@ -1,12 +1,95 @@
-﻿using System.Linq;
+﻿using System;
 
 namespace BankOCR
 {
     public class OCRConverter
     {
+        private readonly int[] _weights =
+            {
+                1, 2, 1,
+                3, 5, 7,
+                11, 13, 17
+            };
+
+        private const int PrimeOne = 1 * 7 * 17;
+        private const int PrimeTwo = 2 * 5 * 7 * 11 * 13;
+        private const int PrimeThree = 2 * 5 * 7 * 13 * 17;
+        private const int PrimeFour = 3 * 5 * 7 * 17;
+        private const int PrimeFive = 2 * 3 * 5 * 13 * 17;
+        private const int PrimeSix = 2 * 3 * 5 * 11 * 13 * 17;
+        private const int PrimeSeven = 2 * 7 * 17;
+        private const int PrimeEight = 2 * 3 * 5 * 7 * 11 * 13 * 17;
+        private const int PrimeNine = 2 * 3 * 5 * 7 * 13 * 17;
+        private const int PrimeZero = 2 * 3 * 7 * 11 * 13 * 17;
+
+        private int[] ConvertWithPrimes(string testValue)
+        {
+            var charValues = new[] { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            const int blockHeight = 4;
+            const int blockLength = 27;
+            const int characterWidth = 3;
+
+            var lineLength = blockLength * blockHeight + blockHeight * Environment.NewLine.Length;
+            if (testValue.Length != lineLength) throw new ArgumentException("The supplied string is improperly formatted.");
+
+            int rowPos = 0;
+            int colPos = 0;
+            while (true)
+            {
+                int currentChar = colPos / characterWidth;
+                int subCharPos = colPos % characterWidth + rowPos * characterWidth;
+                int stringPos = colPos + rowPos * blockLength + Environment.NewLine.Length * rowPos;
+
+                var characterWieght = testValue[stringPos] != ' ' ? _weights[subCharPos] : 1;
+                charValues[currentChar] *= characterWieght;
+
+                colPos++;
+                if (colPos == blockLength)
+                {
+                    rowPos++;
+                    colPos = 0;
+                }
+
+                if (rowPos == blockHeight - 1) break;
+            }
+
+            return charValues;
+        }
+
+        private int ConvertPrimeValueToDigit(int value)
+        {
+            switch (value)
+            {
+                case PrimeOne: return 1;
+                case PrimeTwo: return 2;
+                case PrimeThree: return 3;
+                case PrimeFour: return 4;
+                case PrimeFive: return 5;
+                case PrimeSix: return 6;
+                case PrimeSeven: return 7;
+                case PrimeEight: return 8;
+                case PrimeNine: return 9;
+                case PrimeZero: return 0;
+                default: return value;
+            }
+        }
+
+        private int[] ConvertPrimeValueToAccountNumber(int[] values)
+        {
+            var result = new int[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                result[i] = ConvertPrimeValueToDigit(values[i]);
+            }
+
+            return result;
+        }
+
         public int[] Convert(string testValue)
         {
-            return Enumerable.Empty<int>().ToArray();
+            var primeResult = ConvertWithPrimes(testValue);
+            var finalResult = ConvertPrimeValueToAccountNumber(primeResult);
+            return finalResult;
         }
     }
 }
